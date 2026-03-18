845c629d90a683c995451a2b846b1fa06a1d2b94
dallaechoi <choi032799@gmail.com>
2026-03-19 02:44:42 +0900

    setting: 초기세팅

---
+++ src/test/java/com/example/lucaus26th/Lucaus26thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lucaus26th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lucaus26thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
