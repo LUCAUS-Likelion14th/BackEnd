8cce752fb9429f88d5dcbf43010e4030d85c446b
dallaechoi <choi032799@gmail.com>
2026-03-29 01:38:54 +0900

    fix : basetimeentity 이름 수정

--- src/main/java/com/example/lucaus26th/domain/BaseTimeEntity.java
+++ src/main/java/com/example/lucaus26th/domain/BaseTimeEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BseTimeEntity {
+public class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
