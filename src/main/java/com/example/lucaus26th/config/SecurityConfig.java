997ffff588cfe12a00cf645d1eb60be6c64c9b31
YoonisKim <runheng1209@gmail.com>
2026-03-28 18:20:32 +0900

    feat:폴더 구조 생성 및 소셜로그인 1차 완성(저장x)

---
+++ src/main/java/com/example/lucaus26th/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.example.lucaus26th.config;
+
+import com.example.lucaus26th.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 설정 추가
+        http
+                .cors((SecurityConfig::corsAllow))
+                .csrf(AbstractHttpConfigurer::disable) // 일반은 비활성화
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/join", "/login",
+                                "/oauth2/**", "/login/oauth2/**",
+                                "/h2-console/**", "/error").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                )
+        ;
+
+        return http.build();
+    }
+
+    private static void corsAllow(CorsConfigurer<HttpSecurity> corsConfigurer) {
+        corsConfigurer.configurationSource(request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+
+            configuration.setAllowedMethods(Collections.singletonList("*")); // 모든 메서드 허용
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트에서 오는 요청 허용
+            configuration.setAllowedHeaders(Collections.singletonList("*")); // 모든 헤더 허용
+            configuration.setAllowCredentials(true);
+            configuration.setMaxAge(3600L); // 1시간(3600초) 동안 오는 요청이 처리됨
+
+            return configuration;
+        });
+    }
+}
