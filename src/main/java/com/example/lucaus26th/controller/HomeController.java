edc7cc7cd4dbe4b9d03f7708679c3a5b0178ec16
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2026-04-03 01:19:23 +0900

    feat: 메인홈 컨트롤러 추가

--- src/main/java/com/example/lucaus26th/controller/HomeController.java
+++ src/main/java/com/example/lucaus26th/controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.example.lucaus26th.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "메인 홈", description = "메인 홈에 사용할 API입니다.")
 public class HomeController {
+
 }
