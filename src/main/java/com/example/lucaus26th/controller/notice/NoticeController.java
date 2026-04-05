ddf4f2cea5b586b874e74b80f5df207a947b5c4f
YoonisKim <runheng1209@gmail.com>
2026-04-05 20:15:14 +0900

    refactor:api의 목적에 따른 엔드포인트 분리(admin 여부)

--- src/main/java/com/example/lucaus26th/controller/notice/NoticeController.java
+++ src/main/java/com/example/lucaus26th/controller/notice/NoticeController.java
@@ -11,52 +11,51 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/notice")
 @RequiredArgsConstructor
 public class NoticeController {
 
     private final NoticeService noticeService;
 
     // 공지 생성
-    @PostMapping
+    @PostMapping("/admin/notice")
     public ResponseEntity<NoticeResponseDto> createNotice(@RequestBody NoticeRequestDto request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(noticeService.createNotice(request));
     }
 
     // 공지 전체 조회
-    @GetMapping
+    @GetMapping("/notice")
     public ResponseEntity<List<NoticeResponseDto>> getNotices() {
         return ResponseEntity.ok(noticeService.getALLNotice());
     }
 
     // 공지 수정
-    @PatchMapping("/{noticeId}")
+    @PatchMapping("/admin/notice/{noticeId}")
     public ResponseEntity<NoticeResponseDto> patchNotice(@PathVariable Long noticeId, @RequestBody NoticeRequestDto request) {
         return ResponseEntity.ok(noticeService.patchNotice(noticeId, request));
     }
 
     // 공지 삭제
-    @DeleteMapping("/{noticeId}")
+    @DeleteMapping("/admin/notice/{noticeId}")
     public ResponseEntity<Void> deleteNotice(@PathVariable Long noticeId) {
         noticeService.deleteNotice(noticeId);
         return ResponseEntity.noContent().build();
     }
 
     // 상세 조회
-    @GetMapping("/{noticeId}")
+    @GetMapping("notice/{noticeId}")
     public ResponseEntity<NoticeResponseDto> getNotice(@PathVariable Long noticeId) {
         return ResponseEntity.ok(noticeService.getNotice(noticeId));
     }
 
     // 중요 반전
-    @PatchMapping("/{noticeId}/important")
+    @PatchMapping("/admin/notice/{noticeId}/important")
     public ResponseEntity<NoticeResponseDto> toggleImportant(@PathVariable Long noticeId) {
         return ResponseEntity.ok(noticeService.toggleImportant(noticeId));
     }
 
     // 실시간 반전
-    @PatchMapping("/{noticeId}/active")
+    @PatchMapping("/admin/notice/{noticeId}/active")
     public ResponseEntity<NoticeResponseDto> toggleActive(@PathVariable Long noticeId) {
         return ResponseEntity.ok(noticeService.toggleActive(noticeId));
     }
