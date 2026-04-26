9fd50d622259fced1e15519c28749ee5ac7d861f
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2026-04-27 02:04:33 +0900

    fix: 푸드트럭 s3 이미지 업로드 적용

--- src/main/java/com/example/lucaus26th/dto/request/foodTruck/FoodTruckRequestDto.java
+++ src/main/java/com/example/lucaus26th/dto/request/foodTruck/FoodTruckRequestDto.java
@@ -1,15 +1,22 @@
 package com.example.lucaus26th.dto.request.foodTruck;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class FoodTruckRequestDto {
     private String name;
     private Long locationId;
     private String location;
-    private String image;
+
+    @Schema(type = "string", format = "binary", description = "푸드트럭 이미지 파일")
+    private MultipartFile image;
+
     private String bestMenu;
     private String foodTruckInfo;
 }
