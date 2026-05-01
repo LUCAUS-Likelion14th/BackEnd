346163e8e0b25bb2d43e4f7d64332f87cc9256cb
YoonisKim <runheng1209@gmail.com>
2026-05-02 01:24:46 +0900

    feat:CacheConfig 추가

---
+++ src/main/java/com/example/lucaus26th/config/CacheConfig.java
@@ -0,0 +1,43 @@
+package com.example.lucaus26th.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+        RedisCacheConfiguration tenMinutesCache = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(RedisSerializer.string()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(RedisSerializer.json()));
+
+        RedisCacheConfiguration oneHourCache = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(RedisSerializer.string()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(RedisSerializer.json()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(tenMinutesCache)
+                .withCacheConfiguration("cache_10min", tenMinutesCache)
+                .withCacheConfiguration("cache_1hour", oneHourCache)
+                .build();
+    }
+}
