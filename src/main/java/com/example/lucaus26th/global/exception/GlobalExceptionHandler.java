1abea373d58e64b7906c4da59ef5856eaecbb1e0
dallaechoi <choi032799@gmail.com>
2026-05-03 21:02:19 +0900

    Merge pull request #32 from LUCAUS-Likelion14th/feat/redis

--- src/main/java/com/example/lucaus26th/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/lucaus26th/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.example.lucaus26th.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// BusinessException을 잡아서 그 안의 errorcode를 응답으로 바꾸어 줌
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 우리가 정의한 BusinessException을 처리
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponseDto> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
@@ -18,20 +18,22 @@ public class GlobalExceptionHandler {
                 .body(ErrorResponseDto.of(errorCode));
     }
 
-    // 잘못된 요청값 처리
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.warn("잘못된 요청값", e);
+
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)  // 400
+                .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponseDto(
                         ErrorCode.INVALID_INPUT_VALUE.getCode(),
                         e.getMessage()
                 ));
     }
 
-    // 예상하지 못한 모든 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
+        log.error("예상하지 못한 서버 예외 발생", e);
+
         return ResponseEntity
                 .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
                 .body(ErrorResponseDto.of(ErrorCode.INTERNAL_SERVER_ERROR));
