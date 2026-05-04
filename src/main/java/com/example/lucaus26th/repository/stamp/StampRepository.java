5bc41c8d3512f5fb351610a850b97d4fbf254123
dallaechoi <choi032799@gmail.com>
2026-05-05 03:18:39 +0900

    feat : 도장판 관련 레포지토리 생성 및 도장판 할 부스 등록 서비스 구현

---
+++ src/main/java/com/example/lucaus26th/repository/stamp/StampRepository.java
@@ -0,0 +1,13 @@
+package com.example.lucaus26th.repository.stamp;
+
+import com.example.lucaus26th.domain.Member;
+import com.example.lucaus26th.domain.booth.Booth;
+import com.example.lucaus26th.domain.stamp.Stamp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StampRepository extends JpaRepository<Stamp,Long> {
+    boolean existsByBoothAndMember(Booth booth, Member member);
+    List<Stamp> findByMember(Member member);
+}
---
+++ src/main/java/com/example/lucaus26th/service/stamp/StampService.java
@@ -0,0 +1,4 @@
+package com.example.lucaus26th.service.stamp;
+
+public class StampService {
+}
