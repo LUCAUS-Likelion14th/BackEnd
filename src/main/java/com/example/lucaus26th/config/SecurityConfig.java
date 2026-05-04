8156cd9842dfb07c981d05e2aab93c2b0921950b
dallaechoi <choi032799@gmail.com>
2026-05-04 11:50:14 +0900

    feat : cors 설정에 프론트 배포 도메인 추가

--- src/main/java/com/example/lucaus26th/config/SecurityConfig.java
+++ src/main/java/com/example/lucaus26th/config/SecurityConfig.java
@@ -92,7 +92,8 @@ public class SecurityConfig {
             CorsConfiguration configuration = new CorsConfiguration();
 
             configuration.setAllowedMethods(Collections.singletonList("*"));
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
+                    "https://lucaus-liart.vercel.app"));
             configuration.setAllowedHeaders(Collections.singletonList("*"));
             configuration.setAllowCredentials(true);
             configuration.setMaxAge(3600L);
