641d8a8e655181c75624b996f5c5f4d449f82589
YoonisKim <runheng1209@gmail.com>
2026-05-06 19:39:33 +0900

    feat:health check 반복하도록 설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,16 +4,20 @@ echo "> lucaus 서비스 재시작"
 
 sudo systemctl restart lucaus
 
-echo "> 서비스 시작 대기"
-sleep 20
+echo "> health check 시작"
 
-echo "> health check 확인"
+for i in {1..24}
+do
+  STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/health)
 
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/health)
-
-if [ "$STATUS" = "200" ]; then
+  if [ "$STATUS" = "200" ]; then
     echo "> 애플리케이션 실행 성공"
-else
-    echo "> 애플리케이션 실행 실패"
-    exit 1
-fi
+    exit 0
+  fi
+
+  echo "> 아직 startup 중... ($i)"
+  sleep 5
+done
+
+echo "> 애플리케이션 실행 실패"
+exit 1
